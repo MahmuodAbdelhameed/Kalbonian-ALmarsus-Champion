88daaac50ea7128e94fbc1c8e2bf8deca6375734
MahmuodAbdelhameed <54806019+MahmuodAbdelhameed@users.noreply.github.com>
2022-04-01 21:41:45 +0200

    Add files via upload

---
+++ first-challenge.py
@@ -0,0 +1,15 @@
+print('Challenge 1:')
+# A message for user
+massage = "This is going to be tricky;)"
+Massage = "Very tricky!"
+
+print(massage)
+
+# Ferform mathematical operations
+reSult = 2 ** 3
+
+print('2 ** 3 =',reSult)
+result = 5 - 3
+print('5 - 3 =',result)
+
+print('Challenge complete!')
---
+++ strings.py
@@ -0,0 +1,10 @@
+# Great user
+name = 'mahmuod'
+
+print("It's nice to meet you",name,)
+answer = input("are you enjoying the course?\n")
+
+if answer=="yes":
+    print("That's good to hear")
+else:
+    print('thanks for answers')
